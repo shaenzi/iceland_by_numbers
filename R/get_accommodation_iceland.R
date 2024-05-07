1cef27951474963b5084486d93505ecec2cc7016
Sara Hänzi <sara.programs@proton.me>
2024-05-08 00:07:17 +0200

    first plots of icelandic numbers (accommodation

---
+++ R/get_accommodation_iceland.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(pxweb)
+# documentation for this package https://ropengov.github.io/pxweb/articles/pxweb.html or https://github.com/rOpenGov/pxweb
+# for the underlying API https://www.scb.se/api_en/
+
+
+get_accommodation_by_region <- function() {
+  url <- "https://px.hagstofa.is:443/pxen/api/v1/en/Atvinnuvegir/ferdathjonusta/gisting/3_allartegundirgististada/SAM01601.px"
+
+  pxq <- pxweb_query(here::here("json", "query_accommodation_region.json"))
+
+  px <- pxweb_get(url = url,
+                  query = pxq)
+
+  df <- as.data.frame(px) |>
+    janitor::clean_names() |>
+    mutate(year = as.numeric(year),
+           month = factor(month,
+                          levels = c("January", "February", "March", "April",
+                                     "May", "June", "July", "August",
+                                     "September", "October", "November",
+                                     "December"))) |>
+    rename(value = overnight_stays_and_arrivals_in_all_types_of_registered_accommodation_1998)
+
+  return(df)
+}
+
+get_accommodation_by_origin <- function() {
+  url <- "https://px.hagstofa.is:443/pxen/api/v1/en/Atvinnuvegir/ferdathjonusta/gisting/3_allartegundirgististada/SAM01601.px"
+
+  pxq <- pxweb_query(here::here("json", "query_accommodation_origin.json"))
+
+  px <- pxweb_get(url = url,
+                  query = pxq)
+
+  return(as.data.frame(px))
+}
+
